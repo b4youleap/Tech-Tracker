36067ddbf4e9c2ae4885c18d47ae7fb587341641
Bruce MacDonald <b4youleap@gmail.com>
2016-09-24 21:44:20 -0700

    Following TOH - ready for Muliple Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+export class Technology {
+  id: string;
+  name: string;
+  version: string;
+  stage: string;
+}
+
+const TECHNOLOGIES: Technology[] = [
+    { id: 'ts101', name: 'OS X', version: '10.11', stage: 'Standard' },
+    { id: 'ts102', name: 'Windows', version: '10', stage: 'Standard' },
+    { id: 'ts103', name: 'Touch HW', version: 'x', stage: 'Standard' },
+    { id: 'ts104', name: 'Local Hypervisor', version: 'x', stage: 'Research' },
+    { id: 'ts105', name: 'Hardware RFP', version: 'x', stage: 'Research' },
+    { id: 'ts106', name: 'Lenovo', version: 'x', stage: 'Retired' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Technology Roadmap - Technology Listing';
+  technologies = TECHNOLOGIES;
+  selectedTechnology: Technology;
+
+  onSelect(technology: Technology): void {
+    this.selectedTechnology = technology;
+  }
 }
+
+/*  Conversion from 'Tour of Heroes tutorial'
+
+- lowercase hero = technology
+- lowercase heroes = technologies
+- uppercase HEROES = TECHNOLOGIES
+- mixedcase Hero = Technology
+- mixedcase selectedHero = selectedTechnology
+
+*/
